3864c75cc8d5e2a111d7d7c20e6bf678d947e4a1
andr3-zao <63406068+andr3-zao@users.noreply.github.com>
2020-09-30 07:48:56 -0300

    game

---
+++ Chute o numero.py
@@ -0,0 +1,53 @@
+import random
+import PySimpleGUI as sg
+
+
+class ChuteONumero:
+    def __init__(self):
+        self.valor_aleatorio = 0
+        self.valor_minimo = 1
+        self.valor_maximo = 50
+        self.tentar_novamente = True
+
+    def Iniciar(self):
+        sg.theme('DarkAmber')
+        # Layout
+        layout = [
+            [sg.Text('Seu Chute', size=(39, 0))],
+            [sg.Input(size=(18, 0), key='ValorChute')],
+            [sg.Button('Chutar!')],
+            [sg.Output(size=(39, 10))]
+        ]
+        # criar uma janela
+        self.janela = sg.Window('Chute o numero!', layout=layout)
+        self.GerarNumeroAleatorio()
+        try:
+            while True:
+                # receber os valores
+                self.evento, self.valores = self.janela.Read()
+                if self.evento == sg.WIN_CLOSED or self.evento == 'Cancel':
+                    break
+                # Fazer alguma coisa com estes valores
+                if self.evento == 'Chutar!':
+                    self.valor_do_chute = self.valores['ValorChute']
+                    while self.tentar_novamente == True:
+                        if int(self.valor_do_chute) > self.valor_aleatorio:
+                            print('Chute um valor mais baixo!')
+                            break
+                        elif int(self.valor_do_chute) < self.valor_aleatorio:
+                            print('Chute um valor mais alto!')
+                            break
+                        if int(self.valor_do_chute) == self.valor_aleatorio:
+                            self.tentar_novamente = False
+                            print('PARABÉNS VOCÊ ACERTOU!!')
+                            break
+        except:
+            print('Favor digitar apenas números!')
+            self.Iniciar()
+
+    def GerarNumeroAleatorio(self):
+        self.valor_aleatorio = random.randint(self.valor_minimo, self.valor_maximo)
+
+
+chute = ChuteONumero()
+chute.Iniciar()
